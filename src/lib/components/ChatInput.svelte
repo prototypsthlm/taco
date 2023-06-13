85d7baebf6d50c397b67f919d62cdbdc178a1dd2
brokelyn <phil.fhs11@googlemail.com>
2023-06-13 11:21:36 +0200

    Add basic layout for sidebar and chat input

---
+++ src/lib/components/ChatInput.svelte
@@ -0,0 +1,3 @@
+<div class="w-full h-80 bg-slate-900">
+  Chat Input
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
-  import "../app.css"
+  import ChatInput from '$lib/components/ChatInput.svelte';
+  import Sidebar from '$lib/components/Sidebar.svelte';
+  import '../app.css';
 </script>
 
-<slot />
+<svelte:head>
+  <title>LLM Portal</title>
+</svelte:head>
+
+<div class="flex h-screen w-screen">
+  <Sidebar />
+  <div class="flex flex-col h-full w-full">
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <ChatInput />
+  </div>
+</div>
