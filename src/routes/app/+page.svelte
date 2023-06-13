c1d57b32e06fa66a52253592a2e24c2bbbaa2e2b
Toni Oriol <tonioriol@gmail.com>
2023-06-13 12:45:13 +0200

    move chat layout inside app route

---
+++ src/routes/app/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import ChatInput from '$lib/components/ChatInput.svelte'
+  import Sidebar from '$lib/components/Sidebar.svelte'
+</script>
+
+<div class="flex h-screen w-screen">
+  <Sidebar />
+  <div class="flex flex-col h-full w-full">
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <ChatInput />
+  </div>
+</div>
