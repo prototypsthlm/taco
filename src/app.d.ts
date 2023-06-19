c6b865963bc836f32856dd6b813f8beba4869913
Toni Oriol <tonioriol@gmail.com>
2023-06-19 19:58:55 +0200

    hook to handle auth cookies

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { User } from '@prisma/client'
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    interface Locals {
+      currentUser: User
+    }
+    // interface PageData {}
+    // interface Platform {}
+  }
 }
 
-export {};
+export {}
---
+++ src/hooks.server.ts
@@ -0,0 +1,34 @@
+import { getUserBySessionId } from '$lib/server/user'
+import type { Handle } from '@sveltejs/kit'
+import { redirect } from '@sveltejs/kit'
+
+const unProtectedRoutes = ['/', '/signin', '/signup']
+
+export const handle: Handle = async ({ event, resolve }) => {
+  // sign-out route
+  if (event.url.searchParams.get('signout')) {
+    await event.cookies.delete('session_id', { path: '/' })
+    return resolve(event)
+  }
+
+  const sessionId = event.cookies.get('session_id')
+
+  // no cookie
+  if (!sessionId) {
+    if (!unProtectedRoutes.includes(event.url.pathname)) {
+      throw redirect(303, '/')
+    }
+  } else {
+    // cookie and trying to access protected route
+    const currentUser = await getUserBySessionId(sessionId)
+
+    if (currentUser) {
+      event.locals.currentUser = currentUser
+    } else {
+      await event.cookies.delete('session_id', { path: '/' })
+      throw redirect(303, '/')
+    }
+  }
+
+  return resolve(event)
+}
