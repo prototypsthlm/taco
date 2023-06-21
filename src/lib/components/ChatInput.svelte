cfafda645c0539b6729e250c4e485c0b99e2b924
brokelyn <phil.fhs11@googlemail.com>
2023-06-21 13:22:36 +0200

    Refactor input method to svelte method and add class for no border

--- src/lib/components/ChatInput.svelte
+++ src/lib/components/ChatInput.svelte
@@ -1,48 +1,49 @@
 <script lang="ts">
-  import Send from 'svelte-material-icons/Send.svelte';
-  import { createEventDispatcher, onMount } from 'svelte';
-  import { browser } from '$app/environment';
+  import Send from 'svelte-material-icons/Send.svelte'
+  import { createEventDispatcher } from 'svelte'
 
-  const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher()
 
   function dispatchMessage() {
     if (text) {
-      dispatch('message', text);
-      text = '';
+      dispatch('message', text)
+      text = ''
     }
   }
 
-  onMount(() => {
-    if (!browser) return;
-
-    const textarea: HTMLTextAreaElement = document.getElementById(
-      'inputTextArea'
-    ) as HTMLTextAreaElement;
-
-    if (textarea) {
-      textarea.addEventListener('input', () => {
-        if (textarea.scrollHeight <= 500) {
-          textarea.style.height = 'auto';
-          textarea.style.height = textarea.scrollHeight + 'px';
-        }
-      });
+  function handleTextAreaSize(inputEvent: any) {
+    const textarea = inputEvent.target as HTMLTextAreaElement;
+    if (textarea.scrollHeight <= 500) {
+      textarea.style.height = 'auto'
+      textarea.style.height = textarea.scrollHeight + 'px'
     }
-  });
+  }
 
-  let text: string = '';
+  let text: string = ''
 </script>
 
 <div class="w-full flex justify-center">
   <div class="flex w-5/6 max-w-6xl">
-    <textarea
-      id="inputTextArea"
+    <div class="flex justify-centermin-h-[4rem] w-full bg-gray-600 rounded-l-xl">
+      <textarea
       rows="1"
       bind:value={text}
+      on:input={handleTextAreaSize}
       placeholder="Type your message"
-      class="w-full p-4 appearance-none text-2xl bg-gray-600 focus:outline-none rounded-l-xl text-white"
-    />
+      class="no-border w-full items-center my-auto resize-none m-4 text-2xl bg-gray-600 text-white"
+      />
+    </div>
     <button on:click={dispatchMessage} class="p-4 pr-14 w-16 rounded-r-xl bg-gray-600 group">
       <Send class="text-white h-10 w-10 opacity-40 group-hover:opacity-95 duration-200" />
     </button>
   </div>
 </div>
+
+<style>
+  .no-border {
+    appearance: none;
+    border: none;
+    outline: none;
+    box-shadow: none;
+  }
+</style>
