c8cef83c251ce2cabc33099d0636a198b2b0c0d8
brokelyn <phil.fhs11@googlemail.com>
2023-06-21 10:55:56 +0200

    Update layout and page html

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,14 +1,2 @@
-<script lang="ts">
-  import ChatInput from '$lib/components/ChatInput.svelte'
-  import Sidebar from '$lib/components/Sidebar.svelte'
-</script>
 
-<div class="flex h-screen w-screen">
-  <Sidebar />
-  <div class="flex flex-col h-full w-full">
-    <div class="flex-grow">
-      <slot />
-    </div>
-    <ChatInput />
-  </div>
-</div>
+<h1 class="text-3xl"> Chat Window </h1>
