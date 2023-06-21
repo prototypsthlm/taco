2156e6ebb5414cb633d8fd53559f921c83da3ee2
Philipp Krause <phil.fhs11@googlemail.com>
2023-06-21 13:22:58 +0200

    LLM-10 create the chat input field (#3)
    * Add material icons
    
    * Add chat input bar with dynamic grow
    
    * Update layout and page html
    
    * Refactor input method to svelte method and add class for no border

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import ChatInput from '$lib/components/ChatInput.svelte'
+  import Sidebar from '$lib/components/Sidebar.svelte'
+</script>
+
+<div class="flex h-screen w-screen">
+  <Sidebar />
+  <div class="flex flex-col h-full w-full">
+    <div class="flex-grow">
+      <slot />
+    </div>
+    <div class="p-4 md:p-8">
+      <ChatInput />
+    </div>
+  </div>
+</div>
