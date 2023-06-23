e3f9036a3ddc947b7338defe61157b0a5b6b028e
Toni Oriol <tonioriol@gmail.com>
2023-06-23 20:14:51 +0200

    add bg color to the theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,16 @@
 import type { Config } from 'tailwindcss'
+import theme = require('tailwindcss/defaultTheme')
 
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
-      colors: {
+      colors: (theme) => ({
         primary: '#5e5e5e',
         secondary: '#333232',
         accent: '#e2e8f0',
-      },
+        bg: theme.colors.gray['900'],
+      }),
     },
   },
   plugins: [require('@tailwindcss/forms')],
