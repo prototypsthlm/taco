df986afe4bc477cf09c585b60f64d3f541a3100c
Toni Oriol <tonioriol@gmail.com>
2023-06-27 18:01:10 +0200

    lint issues

--- src/lib/components/ButtonLink.svelte
+++ src/lib/components/ButtonLink.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  export let href: string;
-  export let text: string;
+  export let href: string
+  export let text: string
 </script>
 
-
-<a href={href} class="p-4 text-2xl font-bold text-accent rounded-xl hover:scale-105 duration-200 bg-primary">
+<a
+  {href}
+  class="p-4 text-2xl font-bold text-accent rounded-xl hover:scale-105 duration-200 bg-primary"
+>
   {text}
 </a>
