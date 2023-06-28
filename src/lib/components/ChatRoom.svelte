3603f3730e832fd3548e8d3ff3e27c982d8ae910
brokelyn <phil.fhs11@googlemail.com>
2023-06-28 10:34:36 +0200

    Fix chatroom data flow and displaying

--- src/lib/components/ChatRoom.svelte
+++ src/lib/components/ChatRoom.svelte
@@ -2,33 +2,35 @@
   import ChatInput from '$lib/components/ChatInput.svelte'
   import Question from '$lib/components/Question.svelte'
   import Answer from '$lib/components/Answer.svelte'
-  import type { Chat } from '@prisma/client'
+  import { enhance } from '$app/forms'
+  import { fade } from 'svelte/transition'
 
   export let chat: any | null
+  let messages: any = []
 
-  function handleMessage(event) {
-    const message = event.detail
-
-    messages.push({
-      question: message,
-      id: 0,
-      model: null,
-      temperature: null,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      answer: null,
-      chatId: 0,
-      authorId: null,
-    })
+  $: if (chat) {
+    messages = chat.messages
   }
 
-  $: messages = chat?.messages || []
+  function addPlaceholderMessage(message: string) {
+    messages = [
+      ...messages,
+      {
+        question: message,
+        id: 0,
+        model: null,
+        temperature: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        answer: null,
+        chatId: 0,
+        authorId: null,
+      },
+    ]
+  }
 </script>
 
 <div class="flex flex-col justify-between items-center h-full w-full">
-  {JSON.stringify(chat, null, 2)}
-
-
   {#if messages.length === 0}
     <div class="flex flex-col justify-center items-center flex-grow h-full">
       <h1 class="text-accent text-5xl font-bold">New Chat!</h1>
@@ -37,15 +39,27 @@
   {:else}
     <div class="flex flex-col w-full h-full overflow-scroll">
       {#each messages as message}
-        <Question text={message.question} author={message.author} />
-        <Answer text={message.answer} />
+        <div in:fade={{ duration: 400 }}>
+          <Question text={message.question} author={message.author} />
+        </div>
+        <div in:fade={{ delay: 400, duration: 400 }}>
+          <Answer text={message.answer} />
+        </div>
       {/each}
     </div>
   {/if}
 
   <div class="m-4 md:m-8 w-full">
-    <form method="POST" action="?/sendMessage" novalidate>
-      <ChatInput on:message={handleMessage} />
+    <form
+      method="POST"
+      action="?/sendMessage"
+      novalidate
+      use:enhance={(formElement) => {
+        const message = String(formElement.formData.get('message'))
+        addPlaceholderMessage(message)
+      }}
+    >
+      <ChatInput />
       {#if chat?.id}
         <input type="hidden" name="chatId" value={chat.id} />
       {/if}
