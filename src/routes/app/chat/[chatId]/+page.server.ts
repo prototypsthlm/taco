e5dec468226943bdd69d0d451ecaa6249b3538a4
brokelyn <phil.fhs11@googlemail.com>
2023-06-28 10:52:51 +0200

    Add chat handler to chatid route

--- src/routes/app/chat/[chatId]/+page.server.ts
+++ src/routes/app/chat/[chatId]/+page.server.ts
@@ -1,4 +1,6 @@
 import { getChatWithRelationsById } from '$lib/entities/chat'
+import { sendMessage } from '$lib/utils/chatting'
+import { fail, type Actions } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ params }) => {
@@ -12,4 +14,24 @@ export const load: PageServerLoad = async ({ params }) => {
       temperature: String(chat?.temperature),
     },
   }
+}
+
+export const actions: Actions = {
+  sendMessage: async ({ request, locals }) => {
+    const data = Object.fromEntries(await request.formData())
+    const userId = locals.currentUser.id
+
+    const res = await sendMessage(data, userId)
+
+    if (res.error) {
+      return fail(res.error.httpStatusCode, {
+        data: res.error.data,
+        errors: res.error.error,
+      })
+    } else {
+      return {
+        chat: res.data,
+      }
+    }
+  },
 }
