78d2bdbf141354f81d11c68d8b747aac6bf32194
brokelyn <phil.fhs11@googlemail.com>
2023-06-28 10:51:36 +0200

    Refactor backend code into utils function

--- src/routes/app/+page.server.ts
+++ src/routes/app/+page.server.ts
@@ -1,67 +1,23 @@
-import { ask } from '$lib/api/openai'
-import {
-  addMessageToChat,
-  createChat,
-  getChatWithRelationsById,
-  storeAnswer,
-} from '$lib/entities/chat'
-import type { Chat } from '@prisma/client'
+import { sendMessage } from '$lib/utils/chatting'
 import { fail } from '@sveltejs/kit'
-import { z, ZodError } from 'zod'
 import type { Actions } from './$types'
 
 export const actions: Actions = {
   sendMessage: async ({ request, locals }) => {
     const data = Object.fromEntries(await request.formData())
-    try {
-      const schema = z
-        .object({
-          message: z.string(),
-          chatId: z.union([z.string(), z.undefined()]),
-        })
-        .parse(data)
+    const userId = locals.currentUser.id
 
-      let chat: Chat
-      if (schema.chatId === undefined) {
-        chat = await createChat(locals.currentUser.id)
-      } else {
-        const chatId = Number(schema.chatId)
-        chat = await getChatWithRelationsById(chatId)
-      }
+    const res = await sendMessage(data, userId)
 
-      const chatWithQuestion = await addMessageToChat(chat, schema.message)
-      const llmResponse = await ask(chatWithQuestion)
-
-      const lastMessage = chatWithQuestion.messages[chatWithQuestion.messages.length - 1]
-      await storeAnswer(lastMessage.id, llmResponse)
-      const updatedChat = await getChatWithRelationsById(chatWithQuestion.id)
-
-      console.log({
-        ...updatedChat,
-        temperature: Number(updatedChat?.temperature),
+    if (res.error) {
+      return fail(res.error.httpStatusCode, {
+        data: res.error.data,
+        errors: res.error.error,
       })
-
+    } else {
       return {
-        chat: {
-          ...updatedChat,
-          temperature: Number(updatedChat?.temperature),
-        },
+        chat: res.data,
       }
-    } catch (error) {
-      console.error(error)
-      if (error instanceof ZodError) {
-        const errors = error.flatten().fieldErrors
-
-        return fail(422, {
-          data,
-          errors,
-        })
-      }
-
-      return fail(500, {
-        data,
-        error,
-      })
     }
   },
 }
