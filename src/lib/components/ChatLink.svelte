0395f40bbda027a465be81ed503252e75b0b8993
brokelyn <phil.fhs11@googlemail.com>
2023-06-28 15:03:01 +0200

    Refactor chat link into extra component

---
+++ src/lib/components/ChatLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Message from 'svelte-material-icons/Message.svelte'
+
+  export let chatId: number
+  export let name: string
+  export let isCurrentPage: boolean
+</script>
+
+<a
+  href="/app/chat/{chatId}"
+  class="flex gap-4 h-full mx-3 rounded-lg py-3 px-4 hover:bg-secondary hover:bg-opacity-75 duration-150"
+  class:bg-secondary={isCurrentPage}
+>
+  <Message class="w-8 h-8 text-accent" />
+  <p class="text-xl text-accent">{name} Nr. {chatId}</p>
+</a>
--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,32 +1,29 @@
 <script lang="ts">
-  import Message from 'svelte-material-icons/Message.svelte'
   import ForumPlus from 'svelte-material-icons/ForumPlus.svelte'
-  import { page } from '$app/stores';  
+  import { page } from '$app/stores'
+  import ChatLink from './ChatLink.svelte'
 
   export let chats = []
 
-  $: sortedChats = chats.sort((a, b) => a.updatedAt - b.updatedAt)
+  $: sortedChats = chats.sort((a, b) => a.updatedAt - b.updatedAt).reverse()
 </script>
 
 <div class="h-full w-96 bg-primary">
   <div class="h-full flex flex-col gap-1 overflow-scroll">
-    <a href="/app" class="mx-3 mt-2 px-4 py-4 rounded-xl hover:bg-secondary flex gap-4">
+    <a
+      href="/app"
+      class="mx-3 mt-2 px-4 py-4 rounded-xl hover:bg-secondary flex gap-4 duration-150"
+    >
       <ForumPlus class="w-8 h-8 text-accent" />
-      <p class="text-white text-2xl">
-        New Chat
-      </p>
+      <p class="text-white text-2xl">New Chat</p>
     </a>
     <hr class="border-accent mx-4 my-4 border-opacity-50" />
     {#each sortedChats as chat}
-        <a
-          href="/app/chat/{chat.id}"
-          class="flex gap-4 h-full mx-3 rounded-lg py-3 px-4 hover:bg-secondary hover:bg-opacity-75"
-          class:bg-secondary={$page.url.pathname == `/app/chat/${chat.id}`}
-        >
-          <Message class="w-8 h-8 text-accent" />
-          <!-- <p class="text-xl text-accent">{chat.updatedAt}</p> -->
-          <p class="text-xl text-accent">{chat.name} Nr. {chat.id}</p>
-        </a>
+      <ChatLink
+        chatId={chat.id}
+        name={chat.name}
+        isCurrentPage={$page.url.pathname == `/app/chat/${chat.id}`}
+      />
     {/each}
   </div>
 </div>
