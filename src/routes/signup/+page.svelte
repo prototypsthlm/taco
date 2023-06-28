43d968bd00c378726f0faa4a1fb9a691dc57b658
Toni Oriol <tonioriol@gmail.com>
2023-06-28 17:00:02 +0200

    invert colors button text

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -80,7 +80,7 @@
         <div>
           <button
             type="submit"
-            class="flex w-full justify-center bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-black shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
+            class="flex w-full justify-center bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
             >Sign up
           </button>
         </div>
