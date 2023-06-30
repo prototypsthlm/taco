7c5d7fcd5e204e5af81748c944afc4f4b19c5cb1
Toni Oriol <tonioriol@gmail.com>
2023-06-30 13:48:48 +0200

    LLM-20: encrypt apiKey in db (#13)
    * move server code to the server folder
    
    * remove unnecessary file
    
    * SECRET_KEY initial setup
    
    and some version package adjustments
    
    * encrypt/decrypt functions
    
    * encrypt/decrypt team api key
    
    * lint issues

--- src/lib/server/api/openai.ts
+++ src/lib/server/api/openai.ts
@@ -1,4 +1,5 @@
-import type { ChatWithRelations } from '$lib/entities/chat'
+import type { ChatWithRelations } from '$lib/server/entities/chat'
+import { decrypt } from '$lib/server/utils/crypto'
 import type { ChatCompletionRequestMessage } from 'openai'
 import { Configuration, OpenAIApi } from 'openai'
 import { ChatCompletionRequestMessageRoleEnum } from 'openai/api'
@@ -15,9 +16,13 @@ export const ask = async (chat: ChatWithRelations) => {
   if (!chat?.owner?.team?.openAiApiKey) {
     throw new Error('Open AI API key is not set!')
   }
-  
-  const client = getClient(chat.owner.team.openAiApiKey)
-  
+
+  if (!process.env.SECRET_KEY) {
+    throw new Error('You must have SECRET_KEY set in your env.')
+  }
+
+  const client = getClient(decrypt(chat.owner.team.openAiApiKey, process.env.SECRET_KEY))
+
   const messages: ChatCompletionRequestMessage[] = chat.messages.flatMap((message) => {
     const question = [
       { role: ChatCompletionRequestMessageRoleEnum.User, content: message.question },
@@ -25,9 +30,9 @@ export const ask = async (chat: ChatWithRelations) => {
     const answer = message.answer
       ? [{ role: ChatCompletionRequestMessageRoleEnum.Assistant, content: message.answer }]
       : []
-      return [...question, ...answer]
+    return [...question, ...answer]
   })
-  
+
   try {
     const completion = await client.createChatCompletion({
       model: chat.model,
@@ -37,15 +42,14 @@ export const ask = async (chat: ChatWithRelations) => {
       ],
       temperature: Number(chat.temperature),
     })
+
+    if (!completion?.data?.choices?.[0].message?.content) {
+      throw new Error('Error getting an answer from API.')
+    }
+
+    return completion.data.choices[0].message.content
   } catch (error) {
     console.error(error)
-    throw new Error('Error getting an answer from API: ' + error?.response?.data?.error)
-
+    throw new Error(`Error getting an answer from API: ${error}`)
   }
-
-  if (!completion.data.choices[0].message?.content) {
-    throw new Error('Error getting an answer from API.')
-  }
-
-  return completion.data.choices[0].message.content
 }
--- src/routes/app/settings/teams/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { getUserWithRelationsById } from '$lib/entities/user'
-import type { PageServerLoad } from './$types'
-
-export const load: PageServerLoad = async ({ locals }) => {
-  const userWithRelations = await getUserWithRelationsById(locals.currentUser.id)
-  return {
-    user: userWithRelations,
-  }
-}
