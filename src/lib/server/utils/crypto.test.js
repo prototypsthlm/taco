7f7d251e017551f55dfb6f87bfa43141ff5758c7
Toni Oriol <tonioriol@gmail.com>
2023-06-30 10:31:07 +0200

    encrypt/decrypt functions

---
+++ src/lib/server/utils/crypto.test.js
@@ -0,0 +1,38 @@
+import { describe, it, expect } from 'vitest'
+import { generateSessionId, encrypt, decrypt } from '$lib/server/utils/crypto'
+
+describe('Crypto functions', () => {
+  it('generateSessionId generates a valid session id', () => {
+    const sessionId = generateSessionId()
+
+    expect(typeof sessionId).toBe('string')
+    expect(sessionId).toHaveLength(64) // randomBytes(32) will generate a string of length 64
+  })
+
+  it('encrypt function encrypts the given text', () => {
+    const secretKey = 'secret'
+    const text = 'test'
+
+    const encrypted = encrypt(text, secretKey)
+
+    expect(encrypted).not.toBe(text)
+  })
+
+  it('decrypt function decrypts the encrypted text back to original', () => {
+    const secretKey = 'secret'
+    const text = 'test'
+
+    const encrypted = encrypt(text, secretKey)
+    const decrypted = decrypt(encrypted, secretKey)
+
+    expect(decrypted).toBe(text)
+  })
+
+  it('decrypt function returns empty string when no encrypted text is provided', () => {
+    const secretKey = 'secret'
+
+    const decrypted = decrypt('', secretKey)
+
+    expect(decrypted).toBe('')
+  })
+})
