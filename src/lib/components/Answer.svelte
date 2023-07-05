76d0e1d7301557c0543cf9463412a9e0e9e7012a
brokelyn <phil.fhs11@googlemail.com>
2023-07-05 11:02:22 +0200

    Add loading animation for api request

--- src/lib/components/Answer.svelte
+++ src/lib/components/Answer.svelte
@@ -1,27 +1,21 @@
 <script lang="ts">
   import { fade } from 'svelte/transition'
+  import { ArrowPathIcon } from '@babeard/svelte-heroicons/solid'
 
   export let text: string | null
-
-  const transitionDelay = 250
 </script>
 
 <div class="w-full min-h-[4rem] p-4 md:p-8 bg-accent bg-opacity-10">
-  {#if text}
-    <div in:fade={{ delay: transitionDelay }} class="flex gap-4 md:gap-8">
-      <p class="text-2xl h-12 w-12 text-center p-2 text-accent bg-green-950 rounded-xl">CG</p>
+  <div in:fade class="flex gap-4 md:gap-8">
+    <p class="text-2xl h-12 w-12 text-center p-2 text-accent bg-green-950 rounded-xl">CG</p>
+    {#if text}
       <p class="max-w-5xl text-xl text-accent">
         {text}
       </p>
-    </div>
-  {:else}
-    <div
-      out:fade={{ duration: transitionDelay }}
-      class="flex items-center justify-center space-x-2"
-    >
-      <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400" />
-      <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400" />
-      <div class="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400" />
-    </div>
-  {/if}
+    {:else}
+      <div in:fade class="flex items-center justify-center space-x-2">
+        <ArrowPathIcon class="h-6 w-6 text-white animate-spin" />
+      </div>
+    {/if}
+  </div>
 </div>
