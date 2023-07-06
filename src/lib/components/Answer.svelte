511695f22afc5d8e48e805be33dcdbe0a7c193ff
brokelyn <phil.fhs11@googlemail.com>
2023-07-06 09:45:23 +0200

    Fix fade in animation applying to all messages

--- src/lib/components/Answer.svelte
+++ src/lib/components/Answer.svelte
@@ -7,11 +7,11 @@
 </script>
 
 <div class="w-full min-h-[4rem] p-4 md:p-8 bg-accent bg-opacity-10">
-  <div in:fade class="flex gap-4 md:gap-8">
+  <div class="flex gap-4 md:gap-8">
     <p class="text-2xl h-12 w-12 text-center p-2 text-accent bg-green-950 rounded-xl">CG</p>
     {#if text}
       <!-- We need to force prose-invert, which is the dark mode for the prose class due to not having a non dark option -->
-      <div class="max-w-5xl text-xl text-accent prose prose-invert">
+      <div in:fade class="max-w-5xl text-xl text-accent prose prose-invert">
         {#await markdownToHtml(text) then parsedText}
           {@html parsedText}
         {/await}
