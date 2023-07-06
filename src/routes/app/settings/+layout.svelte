1b7e443407984b81c80e8f37a1d017cd5c48e36f
Philipp Krause <phil.fhs11@googlemail.com>
2023-07-06 11:00:43 +0200

    LLM-28 refactor sidebar chat history and chat input (#17)
    * Update design of chat sidebar to tailwind ui
    
    * Fix main view not taking full space
    
    * Remove svelte materials icons package
    
    * Add loading animation for api request
    
    * Adjust design of chat input field
    
    * Remove output warning about not needed role
    
    * Add possibility to send messages when pressing enter button
    
    * Fix fade in animation applying to all messages
    
    * Fix some chat scroll behaivior
    
    * Use form binding for submit trigger
    
    * Fix triggering browser submit instead of svelte submit

--- src/routes/app/settings/+layout.svelte
+++ src/routes/app/settings/+layout.svelte
@@ -16,7 +16,6 @@
         <!-- Secondary navigation -->
         <nav class="flex overflow-x-auto py-4">
           <ul
-            role="list"
             class="flex min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8"
           >
             <li>
--- src/routes/app/settings/teams/+page.svelte
+++ src/routes/app/settings/teams/+page.svelte
@@ -13,7 +13,7 @@
 <!-- Deployment list -->
 {#if data?.user?.userTeams}
   <div class="flex flex-col lg:flex-row justify-between divide-x divide-white/5">
-    <ul role="list" class="divide-y divide-white/5 flex-grow">
+    <ul class="divide-y divide-white/5 flex-grow">
       {#each data?.user?.userTeams as userTeam}
         <li class="relative flex items-center space-x-4 px-4 py-4 sm:px-6 lg:px-8">
           <div class="min-w-0 flex-auto">
