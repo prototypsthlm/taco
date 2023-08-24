9b18740968cc8cb18dca595839e85217d1773b43
brokelyn <phil.fhs11@googlemail.com>
2023-08-24 11:13:41 +0200

    Remove load function due to duplication with layout load

--- src/routes/app/settings/teams/+page.server.ts
+++ src/routes/app/settings/teams/+page.server.ts
@@ -1,17 +1,5 @@
-import {
-  changeActiveUserTeam,
-  getUserWithUserTeamsActiveTeamAndChatsById,
-  getUserWithUserTeamsById,
-} from '$lib/server/entities/user'
+import { changeActiveUserTeam, getUserWithUserTeamsById } from '$lib/server/entities/user'
 import { fail, redirect, type Actions } from '@sveltejs/kit'
-import type { PageServerLoad } from './$types'
-
-export const load: PageServerLoad = async ({ locals }) => {
-  const userWithRelations = await getUserWithUserTeamsActiveTeamAndChatsById(locals.currentUser.id)
-  return {
-    user: userWithRelations,
-  }
-}
 
 export const actions: Actions = {
   selectTeam: async ({ request, locals }) => {
