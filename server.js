684ae606e10091d6b21e01cc68e9aea7eca970fd
Toni Oriol <tonioriol@gmail.com>
2023-08-25 12:27:18 +0200

    this might do

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import http from 'http'
 import express from 'express'
-import injectSocketIO from '../socketIoHandler.js'
-import { handler } from '../build/handler.js'
+import injectSocketIO from './socketIoHandler.js'
+import { handler } from './build/handler.js'
 
 const app = express()
 const server = http.createServer(app)
