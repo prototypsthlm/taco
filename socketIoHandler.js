331179037bcab3ef9f68e5702fc6ee640697a237
Toni Oriol <tonioriol@gmail.com>
2023-09-26 18:23:01 +0200

    wip

--- socketIoHandler.js
+++ socketIoHandler.js
@@ -1,29 +1,54 @@
+// socketIoHandler.js
 import { Server } from 'socket.io'
 
 export default function injectSocketIO(server) {
   const io = new Server(server)
 
+  const chatUsers = {}
+  const usersTyping = {}
+
   io.on('connection', (socket) => {
-    let user = `User ${Math.round(Math.random() * 999999)}`
-    console.log(`connection established with user ${user}`)
-    socket.emit('user', user)
+    console.log('connection in')
 
-    socket.on('message', (message) => {
-      io.emit('message', {
-        from: user,
-        message: message,
-        time: new Date().toLocaleString(),
-      })
+    socket.on('join-chat', ({ userId, chatId }) => {
+      console.log('on.join-chat', { userId, chatId })
+
+      chatUsers[chatId] = [...new Set([...(chatUsers[chatId] || []), userId])]
+
+      socket.emit('connected-users-changed', chatUsers)
+      console.log(`socket.emit('connected-users-changed', ${JSON.stringify(chatUsers)})`)
     })
 
-    socket.on('typing', () => {
-      io.emit('typing', user)
+    socket.on('start-typing', ({ userId, chatId }) => {
+      console.log('on.start-typing', { userId, chatId })
+      usersTyping[chatId] = [...new Set([...(usersTyping[chatId] || []), userId])]
+      socket.emit('users-typing-changed', usersTyping)
+      console.log(`socket.emit('users-typing-changed', ${JSON.stringify(usersTyping)})`)
     })
 
-    socket.on('stopped-typing', () => {
-      io.emit('stopped-typing', user)
+    socket.on('stop-typing', ({ userId, chatId }) => {
+      console.log('on.stop-typing', { userId, chatId })
+      usersTyping[chatId] = (usersTyping[chatId] || []).filter((x) => x !== userId)
+      socket.emit('users-typing-changed', usersTyping)
+      console.log(`socket.emit('users-typing-changed', ${JSON.stringify(usersTyping)})`)
+    })
+
+    socket.on('leave-chat', ({ userId, chatId }) => {
+      console.log('on.leave-chat', { userId, chatId })
+
+      chatUsers[chatId] = (chatUsers[chatId] || []).filter((x) => x !== userId)
+      socket.emit('connected-users-changed', chatUsers)
+
+      console.log(`socket.emit('connected-users-changed', ${JSON.stringify(chatUsers)})`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('on.disconnect and removeAllListeners')
+      socket.removeAllListeners('connection')
+      socket.removeAllListeners('join-chat')
+      socket.removeAllListeners('start-typing')
+      socket.removeAllListeners('stop-typing')
+      socket.removeAllListeners('leave-chat')
     })
   })
-
-  console.log('SocketIO injected')
 }
