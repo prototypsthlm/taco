009aab6dcfb79cfc9f963881ed5329778bdc6cde
Toni Oriol <tonioriol@gmail.com>
2023-10-03 16:44:29 +0200

    add missing cascade deletes to the ChatUser table

---
+++ prisma/migrations/20231003144147_add_cascades_to_the_chat_user_table/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "ChatUser" DROP CONSTRAINT "ChatUser_chatId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ChatUser" DROP CONSTRAINT "ChatUser_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "ChatUser" ADD CONSTRAINT "ChatUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ChatUser" ADD CONSTRAINT "ChatUser_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
