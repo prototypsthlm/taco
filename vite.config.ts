1c42bc35502acc491cf61865647e20c174ab72ed
Toni Oriol <tonioriol@gmail.com>
2023-10-05 17:04:18 +0200

    reformat

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sentrySvelteKit } from '@sentry/sveltekit'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
 import { webSocketServer } from './webSocketPluginVite'
 
 export default defineConfig({
-  plugins: [sentrySvelteKit({
-    sourceMapsUploadOptions: {
-      org: "prototyp-vm",
-      project: "taco"
-    }
-  }), sveltekit(), webSocketServer],
+  plugins: [
+    sentrySvelteKit({
+      sourceMapsUploadOptions: {
+        org: 'prototyp-vm',
+        project: 'taco',
+      },
+    }),
+    sveltekit(),
+    webSocketServer,
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
