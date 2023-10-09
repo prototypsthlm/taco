79df12e2c5386f06fe52af364547f3926e1f4dde
Toni Oriol <tonioriol@gmail.com>
2023-10-09 12:59:16 +0200

    remove unused import

--- src/lib/components/TeamMemberList.svelte
+++ src/lib/components/TeamMemberList.svelte
@@ -2,7 +2,6 @@
   import { enhance } from '$app/forms'
   import Alert from '$lib/components/Alert.svelte'
   import Avatar from '$lib/components/Avatar.svelte'
-  import Gravatar from '$lib/components/Gravatar.svelte'
   import { ArrowDownIcon, ChevronUpIcon, TrashIcon } from '@babeard/svelte-heroicons/solid'
 
   const formatDate = (date: Date) =>
