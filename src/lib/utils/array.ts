b386519db2fc379e8c0780c4d168c597cbfb365e
Toni Oriol <tonioriol@gmail.com>
2023-10-13 18:46:45 +0200

    asyncReduce util

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -13,3 +13,22 @@ export const findOrFail = <T>(
   }
   return found
 }
+
+export type AsyncReducer<T, U = T> = (
+  accumulator: U,
+  currentValue: T,
+  index: number,
+  array: T[]
+) => Promise<U>
+
+export const asyncReduce = async <T, U = T>(
+  array: T[],
+  asyncCallback: AsyncReducer<T, U>,
+  initialValue: U
+): Promise<U> => {
+  return array.reduce(
+    async (accumulatorPromise: Promise<U>, currentValue: T, index: number): Promise<U> =>
+      await asyncCallback(await accumulatorPromise, currentValue, index, array),
+    Promise.resolve(initialValue)
+  )
+}
