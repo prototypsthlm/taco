46eea15e204f7faf94de48f5b03e9cb0e594a790
Toni Oriol <tonioriol@gmail.com>
2023-10-30 12:33:48 +0100

    center text even on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@
 
 <div class="bg-gray-900 py-24 sm:py-32 min-h-screen flex flex-col justify-center">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
-    <div class="mx-auto max-w-2xl lg:text-center">
+    <div class="mx-auto max-w-2xl text-center">
       <div class="text-5xl md:text-7xl mb-12 md:mb-4">
         <TacoIcon class="w-24 md:w-32 dark" />
       </div>
