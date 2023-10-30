ce0f8fa43365eb69eb46035ac8b478cdd6c089a7
Toni Oriol <tonioriol@gmail.com>
2023-10-30 13:41:35 +0100

    fix logo height issues and add favicon for both light and dark

--- src/lib/components/icons/TacoIcon.svelte
+++ src/lib/components/icons/TacoIcon.svelte
@@ -2,7 +2,7 @@
   import logo from '$lib/assets/logo.png'
 </script>
 
-<div class="{$$props.class} flex gap-4 items-center justify-center m-auto">
-  <img class="invert dark:invert-0" src={logo} alt="TACO Logo" />
+<div class="{$$props.class} flex gap-4 items-center">
+  <img class="h-full invert dark:invert-0" src={logo} alt="TACO Logo" />
   <h1 class="font-bold tracking-widest text-black dark:text-accent">TACO</h1>
 </div>
