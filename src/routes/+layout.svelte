741a66f1e08224dfebea930051042ae05bc6fbbb
Erik Lindgren <erik.lindgren@prototyp.se>
2023-12-04 22:01:26 +0100

    Make og:image an absolute url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import '../app.css'
+  import { PUBLIC_SITE_URL } from '$env/static/public'
 
   let faviconPath = '/favicon.png'
 
@@ -28,6 +29,10 @@
 <svelte:head>
   <link id="favicon" rel="icon" href={faviconPath} type="image/png" />
   <title>TACO</title>
+  <meta property="og:title" content="TACO" />
+  <meta property="og:description" content="A free and open source ChatGPT frontend" />
+  <meta property="og:image" content={`${PUBLIC_SITE_URL}/share.png`} />
+  <meta property="og:type" content="website" />
 </svelte:head>
 
 <slot />
