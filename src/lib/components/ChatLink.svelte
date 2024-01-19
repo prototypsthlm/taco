b8032f3d16590e30c8125f5aa5b36b6fa4b30dd1
Leila <leila.benhalima2@gmail.com>
2024-01-19 13:56:21 +0100

    104-change-the-word-fork-it-to-clone-including-icon
    * Change 'fork' to 'clone' and change icon
    
    * Change copy of modal after clicking 'Clone'
    
    * replace fork with clone

--- src/lib/components/ChatLink.svelte
+++ src/lib/components/ChatLink.svelte
@@ -3,13 +3,16 @@
   import { page } from '$app/stores'
   import AvatarGroup from '$lib/components/AvatarGroup.svelte'
   import DeleteChatModal from '$lib/components/DeleteChatModal.svelte'
-  import ForkIcon from '$lib/components/icons/ForkIcon.svelte'
   import ModalConfirm from '$lib/components/ModalConfirm.svelte'
   import ShareChatModal from '$lib/components/ShareChatModal.svelte'
   import type { UserWithUserTeamsActiveTeamAndChats } from '$lib/server/entities/user'
   import { isSidebarOpen } from '$lib/stores/general'
   import { getTimeSince } from '$lib/utils/timeConverter'
-  import { ChatBubbleLeftIcon, UserGroupIcon } from '@babeard/svelte-heroicons/solid'
+  import {
+    Square2StackIcon,
+    ChatBubbleLeftIcon,
+    UserGroupIcon,
+  } from '@babeard/svelte-heroicons/solid'
 
   export let chat: UserWithUserTeamsActiveTeamAndChats['sharedChats'][number]['chat']
   const name = chat.name || 'New Chat'
@@ -19,8 +22,8 @@
   $: href = `/app/chats/${chat.id}`
   $: isLinkActive = $page.url.href.endsWith(`/app/chats/${chat.id}`)
 
-  let forkForm: HTMLFormElement
-  let forkInput: HTMLInputElement
+  let cloneForm: HTMLFormElement
+  let cloneInput: HTMLInputElement
 </script>
 
 <a href={isLinkActive ? null : href} title={name} on:click={() => isSidebarOpen.set(false)}>
@@ -37,6 +40,7 @@
         <ChatBubbleLeftIcon class="h-6 w-6 text-white flex-shrink-0" />
       {/if}
       <h3 class="flex-auto truncate text-sm font-semibold leading-6 text-white">{name}</h3>
+
       <time datetime={chat.updatedAt.toISOString()} class="flex-none text-xs text-gray-600"
         >{updatedAtShorthand}</time
       >
@@ -51,40 +55,40 @@
           {chat.roleContent}
         </p>
         <ShareChatModal {user} {chat} />
-        <ModalConfirm initialFocus={forkInput} on:confirm={() => forkForm.requestSubmit()}>
-          <button class="block" type="button" title="Fork it" slot="trigger">
-            <ForkIcon class="h-5 w-5 text-gray-500 hover:text-green-500 duration-200" />
+        <ModalConfirm initialFocus={cloneInput} on:confirm={() => cloneForm.requestSubmit()}>
+          <button class="block" type="button" title="Clone" slot="trigger">
+            <Square2StackIcon class="h-5 w-5 text-gray-500 hover:text-green-500 duration-200" />
           </button>
-          <svelte:fragment slot="title">Do you want to fork the chat?</svelte:fragment>
+          <svelte:fragment slot="title">Do you want to clone the chat?</svelte:fragment>
           <svelte:fragment slot="body">
             <form
               method="post"
-              action="/app/chats/{chat.id}?/forkChat"
-              bind:this={forkForm}
+              action="/app/chats/{chat.id}?/cloneChat"
+              bind:this={cloneForm}
               use:enhance
               class="w-full"
             >
               <div class="mt-2 max-w-xl text-sm text-gray-500">
-                <p>Choose a name for the new forked chat.</p>
+                <p>Choose a name for the new cloned chat.</p>
               </div>
               <div class="mt-5 sm:flex sm:items-center">
                 <div class="w-full sm:max-w-xl flex">
                   <label class="sr-only" for="newName">New Name</label>
                   <input
-                    bind:this={forkInput}
+                    bind:this={cloneInput}
                     type="text"
                     name="newName"
                     id="newName"
                     class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="A new name for the forked chat."
-                    value={`Forked: ${name}`}
+                    placeholder="A new name for the cloned chat."
+                    value={`Cloned: ${name}`}
                   />
                 </div>
               </div>
               <input type="hidden" name="chatId" value={chat.id} />
             </form>
           </svelte:fragment>
-          <svelte:fragment slot="confirm">Fork</svelte:fragment>
+          <svelte:fragment slot="confirm">Clone</svelte:fragment>
         </ModalConfirm>
         <DeleteChatModal {chat} />
       </div>
