172983c3dd0829b04958be49a8ca683e8864124d
leilabb <leila.benhalima2@gmail.com>
2024-02-05 12:56:39 +0100

    edit test file

--- src/lib/utils/time.test.ts
+++ src/lib/utils/time.test.ts
@@ -8,34 +8,29 @@ const dateFromLastYear = new Date(
   currentDate.getDate()
 )
 const twoYearsAgo = new Date(currentDate)
-const twoYearsAhead = new Date(currentDate)
 twoYearsAgo.setFullYear(currentDate.getFullYear() - 2)
+
+const twoYearsAhead = new Date(currentDate)
 twoYearsAhead.setFullYear(currentDate.getFullYear() + 2)
 
 describe(isLastYear, () => {
-  it('returns true for a date from the previous year'),
-    () => {
-      expect(isLastYear(dateFromLastYear)).toBe(true)
-    }
+  it('returns true for a date from the previous year', () => {
+    expect(isLastYear(dateFromLastYear)).toBe(true)
+  })
 
-  it('returns false for a date of the current year'),
-    () => {
-      expect(isLastYear(currentDate)).toBe(false)
-    }
+  it('returns false for a date of the current year', () => {
+    expect(isLastYear(currentDate)).toBe(false)
+  })
 })
 
 describe(categorizeDate, () => {
-  it('returns the year two years ago'),
-    () => {
-      expect(categorizeDate(twoYearsAgo)).toBe(2022)
-    },
-    it('returns "today" '),
-    () => {
+  it('returns the year two years ago', () => {
+    expect(categorizeDate(twoYearsAgo)).toBe(2022)
+  }),
+    it('returns "today" ', () => {
       expect(categorizeDate(currentDate)).toBe('today')
-    },
-
-    it('returns "futureDate" '),
-    () => {
+    }),
+    it('returns "futureDate" ', () => {
       expect(categorizeDate(twoYearsAhead)).toBe('futureDate')
-    }
+    })
 })
