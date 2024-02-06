c4785a6e486e0cc8987b0663ead587eac8e1efcb
Toni Oriol <tonioriol@gmail.com>
2024-02-06 22:32:32 +0100

    try again

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,11 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'npm run build && npm run preview',
-    port: 3000,
+    command: 'npm run build && node server.js',
+    url: 'http://localhost:3000',
+  },
+  use: {
+    baseURL: 'http://localhost:3000',
   },
   testDir: 'tests',
   testMatch: /(.+\.)?(test|spec)\.[jt]s/,
