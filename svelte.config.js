da66f048c842b61bed5ea4e8155a9eab862d2ac2
Toni Oriol <tonioriol@gmail.com>
2024-03-25 18:26:41 +0100

    try smthn

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ export default {
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
     csrf: {
-      checkOrigin: process.env.NODE_ENV !== 'test',
+      checkOrigin: false,
     },
   },
 }
