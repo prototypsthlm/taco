ba350c47a6d9e8781434e75563f2ec7e34c8a237
Joakim Unge <ashba@unge.se>
2024-08-26 15:17:14 +0200

    feat: add data migration to encrypt existing chats

---
+++ prisma/migrations/20240826081237_add_encrypted_column_to_chat/data-migration.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client'
+import { encrypt } from '../../helpers.ts'
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.$transaction(async (tx) => {
+    const unencryptedChats = await tx.chat.findMany({
+      where: {
+        encrypted: false,
+      },
+      include: {
+        messages: true,
+      },
+    })
+    for (const chat of unencryptedChats) {
+      const key = process.env.SECRET_KEY
+      if (!key) {
+        throw new Error('SECRET_KEY is not set')
+      }
+      const messages = chat.messages
+      const encryptedMessages = messages.map((message) => {
+        return {
+          ...message,
+          question: encrypt(message.question, key),
+          answer: message.answer ? encrypt(message.answer, key) : null,
+        }
+      })
+      await tx.chat.update({
+        where: { id: chat.id },
+        data: {
+          encrypted: true,
+          messages: {
+            updateMany: encryptedMessages.map((message) => ({
+              where: { id: message.id },
+              data: {
+                question: message.question,
+                answer: message.answer,
+              },
+            })),
+          },
+        },
+      })
+    }
+  })
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => await prisma.$disconnect())
