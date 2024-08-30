9ada03ad18840cbacbd4400f0b903180040a8ad2
Christina Sonebo <christina.sonebo@prototyp.se>
2024-08-30 15:55:56 +0200

    feat: show alert for success or failure to save default model

--- src/lib/components/TeamDefaultModel.svelte
+++ src/lib/components/TeamDefaultModel.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { enhance } from '$app/forms'
   import SmallCards from '$lib/components/SmallCards.svelte'
   import type { Model } from '$lib/server/api/openai'
+  import Alert from './Alert.svelte'
 
   export let models: Model[]
   export let model: string
+  export let form: Record<string, string> = {}
 </script>
 
 <div class="px-4 sm:px-6 lg:px-8 max-w-6xl py-10">
@@ -13,7 +16,7 @@
       The model used per default when a member of your team starts a new chat.
     </p>
   </div>
-  <form method="post" action="?/updateTeamModel">
+  <form method="post" action="?/updateTeamModel" use:enhance>
     <SmallCards
       loading={false}
       bind:value={model}
@@ -23,10 +26,16 @@
       class="mb-4"
     />
     <input type="hidden" name="model" value={model} />
-    <button
-      type="submit"
-      class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-      >Save</button
-    >
+    <div class="flex gap-4">
+      <button
+        type="submit"
+        class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+        >Save</button
+      >
+      <Alert
+        type={(form?.error && 'error') || (form?.success && 'success') || 'warning'}
+        title={form?.error || form?.success}
+      />
+    </div>
   </form>
 </div>
