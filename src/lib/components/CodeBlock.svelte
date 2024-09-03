e060d5007c63ceba7d2e0af6225736838c82269a
Joakim Unge <ashba@unge.se>
2024-09-03 11:04:20 +0200

    feat: update styling in codeblock and show lang

--- src/lib/components/CodeBlock.svelte
+++ src/lib/components/CodeBlock.svelte
@@ -10,7 +10,7 @@
   let codeElement: HTMLElement;
 
   function copyToClipboard() {
-    navigator.clipboard.writeText(token.text)
+    navigator.clipboard.writeText(token.text);
   }
 
   onMount(() => {
@@ -21,9 +21,16 @@
 </script>
 
 <div class="relative">
-  <button class="absolute top-2 right-2 bg-gray-800 text-white p-1 rounded" on:click={copyToClipboard} aria-label="Copy">
-    <Square2StackIcon class="w-5 h-5 text-white" aria-hidden="true" />
-  </button>
+  <div class="header-bar flex justify-between items-center px-3 py-3">
+    <div class="text-xs">
+      {token.lang || 'plaintext'}
+    </div>
+
+      <button class="flex items-center text-gray-500 hover:text-white p text-xs" on:click={copyToClipboard} aria-label="Copy">
+        <Square2StackIcon class="w-4 h-4 mr-2" aria-hidden="true" />
+        Copy code
+      </button>
+  </div>
   <pre class="lang-{token.lang}"><code bind:this={codeElement}>{token.text}</code></pre>
 </div>
 
@@ -31,14 +38,13 @@
   .relative {
     position: relative;
   }
-  .absolute {
-    position: absolute;
+  .header-bar {
+    background-color: #1f2937; /* Different color from the code block */
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
   }
-  .top-2 {
-    top: 0.5rem;
-  }
-  .right-2 {
-    right: 0.5rem;
+  .text-xs {
+    font-size: 0.75rem;
   }
   .bg-gray-800 {
     background-color: #2d3748;
@@ -56,12 +62,31 @@
     background-color: #282c34;
     color: #abb2bf;
     padding: 1rem;
-    border-radius: 0.25rem;
+    border-bottom-left-radius: 0.25rem;
+    border-bottom-right-radius: 0.25rem;
+    margin-top: 0; /* Remove margin between header and code block */
   }
   .w-5 {
     width: 1.25rem;
   }
   .h-5 {
     height: 1.25rem;
+  }
+  .flex {
+    display: flex;
+  }
+  .justify-between {
+    justify-content: space-between;
+  }
+  .items-center {
+    align-items: center;
+  }
+  .px-3 {
+    padding-left: 0.75rem;
+    padding-right: 0.75rem;
+  }
+  .py-1 {
+    padding-top: 0.25rem;
+    padding-bottom: 0.25rem;
   }
 </style>
