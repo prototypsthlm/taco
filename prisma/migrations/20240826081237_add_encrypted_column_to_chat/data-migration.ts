4a615d9d1f35347b399fe6b96b8810d9ffcb9949
brokelyn <philipp.krause@droppost.me>
2024-09-26 14:37:19 +0200

    Fix encrypt migrartion timeout issues

--- prisma/migrations/20240826081237_add_encrypted_column_to_chat/data-migration.ts
+++ prisma/migrations/20240826081237_add_encrypted_column_to_chat/data-migration.ts
@@ -3,52 +3,61 @@ import { encrypt } from '../../helpers.ts'
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.$transaction(async (tx) => {
-    const unencryptedChats = await tx.chat.findMany({
-      where: {
-        encrypted: false,
-      },
-      include: {
-        messages: true,
-      },
-    })
-
-    const key = process.env.SECRET_KEY
-
-    if (!key) {
-      throw new Error('SECRET_KEY is not set')
-    }
-
-    for (const chat of unencryptedChats) {
-      console.log('Encrypting chat:', chat.id)
-      const messages = chat.messages
-      const encryptedMessages = messages.map((message) => {
-        return {
-          ...message,
-          question: encrypt(message.question, key),
-          answer: message.answer ? encrypt(message.answer, key) : null,
-        }
-      })
-      await tx.chat.update({
-        where: { id: chat.id },
-        data: {
-          encrypted: true,
-          name: chat.name ? encrypt(chat.name, key) : null,
-          messages: {
-            updateMany: encryptedMessages.map((message) => ({
-              where: { id: message.id },
-              data: {
-                question: message.question,
-                answer: message.answer,
-              },
-            })),
-          },
+  await prisma.$transaction(
+    async (tx) => {
+      const unencryptedChats = await tx.chat.findMany({
+        where: {
+          encrypted: false,
+        },
+        include: {
+          messages: true,
         },
       })
-    }
 
-    console.log('Encrypted chats:', unencryptedChats.length)
-  })
+      const key = process.env.SECRET_KEY
+
+      if (!key) {
+        throw new Error('SECRET_KEY is not set')
+      }
+
+      const encryptTasks: any[] = []
+
+      for (const chat of unencryptedChats) {
+        console.log('Encrypting chat:', chat.id)
+        const messages = chat.messages
+        const encryptedMessages = messages.map((message) => {
+          return {
+            ...message,
+            question: encrypt(message.question, key),
+            answer: message.answer ? encrypt(message.answer, key) : null,
+          }
+        })
+        encryptTasks.push(
+          tx.chat.update({
+            where: { id: chat.id },
+            data: {
+              encrypted: true,
+              name: chat.name ? encrypt(chat.name, key) : null,
+              messages: {
+                updateMany: encryptedMessages.map((message) => ({
+                  where: { id: message.id },
+                  data: {
+                    question: message.question,
+                    answer: message.answer,
+                  },
+                })),
+              },
+            },
+          })
+        )
+      }
+
+      await Promise.all(encryptTasks)
+
+      console.log('Encrypted chats:', unencryptedChats.length)
+    },
+    { timeout: 10000 }
+  )
 }
 
 main()
