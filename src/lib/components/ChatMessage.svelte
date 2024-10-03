b3f39c5ab43bc56639cb683333fae03d44612d29
Philipp Krause <philipp.krause@droppost.me>
2024-10-03 09:01:32 +0200

    Fix code formatting not showing and adjust chat message styling (#212)
    * Fix code syntax highlighting not reactive
    
    * Adjust chat messages styling

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -17,12 +17,25 @@
   const dispatch = createEventDispatcher()
 
   const renderers = {
-    code: CodeBlock
+    code: CodeBlock,
   }
 </script>
 
 <div class="p-4 md:p-8 flex justify-center max-w-full">
-  <div class="max-w-screen-lg w-full flex gap-4 md:gap-8 text-neutral-100">
+  <div class="relative max-w-screen-lg w-full flex justify-end gap-4 md:gap-8 text-neutral-100">
+    <div class="max-w-xl w-full ml-auto flex flex-col flex-grow overflow-x-auto">
+      <span class="font-bold text-right my-2">{message.author?.name}</span>
+      <div class="flex gap-3 items-start justify-end">
+        <button disabled={isDeleting} class="text-white mt-2" on:click={() => dispatch('delete')}>
+          <TrashIcon class="w-5" />
+        </button>
+        <div
+          class="prose prose-invert overflow-x-hidden prose-pre:overflow-x-auto bg-neutral-100 bg-opacity-10 rounded-lg px-4 py-3"
+        >
+          <Markdown source={message.question} />
+        </div>
+      </div>
+    </div>
     <Avatar
       transition={false}
       class="w-10 rounded-xl"
@@ -30,18 +43,9 @@
       dotDisconnectedClass="bottom-0 left-0 h-1.5 w-1.5 bg-gray-900 ring-[2.5px] ring-gray-900 ring-offset-2 ring-offset-gray-400"
       user={message.author}
     />
-    <div class="flex flex-col flex-grow overflow-x-auto">
-      <span class="font-bold">{message.author?.name}</span>
-      <div class="prose prose-invert prose-pre:overflow-x-auto">
-        <Markdown source={message.question} />
-      </div>
-    </div>
-    <button disabled={isDeleting} class="text-white self-start" on:click={() => dispatch('delete')}>
-      <TrashIcon class="w-5" />
-    </button>
   </div>
 </div>
-<div class="p-4 md:p-8 bg-neutral-100 bg-opacity-10 flex justify-center">
+<div class="p-4 md:p-8 flex justify-center">
   <div class="max-w-screen-lg w-full flex gap-4 md:gap-8 text-neutral-100">
     {#if !message?.answer && (!last || !loading)}
       <Alert class="w-full" title="There was an error generating the response" type="error"
@@ -55,8 +59,8 @@
         <span class="font-bold">{message.model}</span>
         {#if message?.answer}
           <!-- We need to force prose-invert, which is the dark mode for the prose class due to not having a non dark option -->
-          <div class="prose prose-invert overflow-x-hidden prose-pre:overflow-x-auto">
-            <Markdown source={message.answer} renderers={renderers}   />
+          <div class="max-w-[825px] prose prose-invert overflow-x-hidden prose-pre:overflow-x-auto">
+            <Markdown source={message.answer} {renderers} />
           </div>
         {:else}
           <div class="flex items-center justify-center space-x-2">
